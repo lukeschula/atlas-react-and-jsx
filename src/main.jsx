6d0eabe8f73be5db7b599a90a13e77359c677758
lukeschula <schulaluke@gmail.com>
2025-01-09 16:56:20 -0600

    Complete Raect setup, and pushing Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
 // Render the App component to the root element
+const div = document.getElementById("root");
+const root = ReactDOM.createRoot(div);
+root.render(<App />);
