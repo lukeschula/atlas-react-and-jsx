9c6967c6c3c906d93999f72069cfb728ca8bdcca
lukeschula <schulaluke@gmail.com>
2025-01-10 10:43:59 -0600

    edit source on SocialLinks so images can be displayed

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -3,14 +3,14 @@ import linkedin from "../assets/linkedin.svg"
 import github from "../assets/github.svg"
 
 
-const SocialLinks = () =>{
+const SocialLinks = () => {
     return (
         <div className="social-links">
             <a href="https://www.linkedin.com/in/luke-schula-480548169/">
-                <img src="../assets/linkedin.svg" alt="LinkedinIcon" />
+                <img src={linkedin} alt="LinkedinIcon" />
             </a>
             <a href="https://github.com/lukeschula">
-                <img src="../assets/github.svg" alt="GithubIcon" />
+                <img src={github} alt="GithubIcon" />
             </a>
         </div>
     );
