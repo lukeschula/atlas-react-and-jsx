e4c4f6f2e5dda2a56981a0e9b7dc3cc9404ed2f2
lukeschula <schulaluke@gmail.com>
2025-01-09 18:06:13 -0600

    Save logic created for rendering conditional images

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import day from "./src/assets/day.svg";
+import evening from "./src/assets/evening.svg";
+import night from "./src/assets/night.svg";
 
 function Greetings() {
-    return (
+    const currentHour = new Date().getHours();
 
+    let greeting = "";
+    let image = null;
+
+    //Logic for greeting based on time of day
+    if (currentHour >= 6 && currentHour <= 11) {
+        greeting = "Good Morning!";
+        image = day;
+    } else if (currentHour >= 12 && currentHour <= 16) {
+        greeting = "Good Afternoon!";
+        image = day;
+    } else if (currentHour >= 17 && currentHour <= 20) {
+        greeting = "Good Evening!";
+        image = evening;
+    } else {
+        greeting = "Good Night!";
+        image = night;
+    }
+
+    return (
         <div>
             <h1>
                 greeting
-                <img src={day} alt="day" />
+                <img className="conditional-greeeting" src={image} alt={"greeting"} />
             </h1>
         </div>
     );
 }
 
-export default Greetings
+export default Greetings;
