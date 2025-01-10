77ebb9df035c6f8576ef8e3db051cf19e0d9beb5
lukeschula <schulaluke@gmail.com>
2025-01-10 09:34:33 -0600

    Created social links component

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,7 +5,12 @@ function SocialLinks() {
     return (
 
         <div className="social-links">
-
+            <a href="https://www.linkedin.com/in/luke-schula-480548169/">
+                <Image src="../assets/linkedin.svg" className="LinkedinIcon" />
+            </a>
+            <a href="https://github.com/lukeschula">
+                <Image src="../assets/github.svg" className="GithubIcon" />
+            </a>
         </div>
     );
 }
