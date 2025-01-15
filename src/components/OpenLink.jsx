ce10cd896bb4a28161d9cb33f8de109c9a397d0e
lukeschula <schulaluke@gmail.com>
2025-01-15 11:23:52 -0600

    Open Link component url passed through link prop to display svg (work from weekend)

---
+++ src/components/OpenLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import open from "../assets/open.svg"
+
+export function OpenLink({link}) {
+    return (
+        <a href="{link}" target="_blank">
+            <img src={open} alt="Open SVG" />
+        </a>
+    );
+}
+
+export default OpenLink;
