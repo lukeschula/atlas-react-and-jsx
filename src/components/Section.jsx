069c24e49932ae855bc1ed0433d161c841e9ccd8
lukeschula <schulaluke@gmail.com>
2025-01-15 10:58:27 -0600

    Pushing Section that returns title and content through children (work revised from the weekend I forgot to push)

---
+++ src/components/Section.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Section({title, children}) {
+    return (
+        <div className="section">
+            <h1>{title}</h1>
+            {children}
+
+        </div>
+    );
+}
+
+export default Section;
